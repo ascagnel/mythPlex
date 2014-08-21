5c81e4f3623a1e0be8cad81c30ca7e318c242173
Andrew Scagnelli <ascagnel@gmail.com>
2014-08-21 19:21:06 -0400

    PEP8
    Reformatting/refactoring to conform to PEP8.

--- mythEx.py
+++ mythEx.py
@@ -7,77 +7,85 @@ import re
 host_url = "10.0.1.10"
 host_port = "6544"
 plex_library_directory = "/home/ascagnel/TV Shows/"
-mythtv_recording_directories = ["/var/media/disk1/Recordings/","/var/media/disk2/Recordings/","/var/media/disk3/Recordings/","/var/media/disk4/Recordings/"]
+mythtv_recording_directories = ["/var/media/disk1/Recordings/",
+                                "/var/media/disk2/Recordings/",
+                                "/var/media/disk3/Recordings/",
+                                "/var/media/disk4/Recordings/"]
 
 for myth_dir in mythtv_recording_directories[:]:
-	if os.path.exists(myth_dir) is not True:
-		print myth_dir + " is not a valid path.  Aborting"
-		quit()
+    if os.path.exists(myth_dir) is not True:
+        print myth_dir + " is not a valid path.  Aborting"
+        quit()
 
 if platform.system() == "Windows":
-	separator = "\\"
+    separator = "\\"
 else:
-	separator = "/"
+    separator = "/"
 
 if os.path.isfile('library') or os.path.islink('library'):
-    library = open('library','r')
-    lib = re.split(",",library.read())
+    library = open('library', 'r')
+    lib = re.split(",", library.read())
     library.close()
 else:
     lib = ""
 
-print "Beginning symlinking.  Looking up from MythTV: http://" + host_url + ":" + host_port + '/Dvr/GetRecordedList'
+url = "http://" + host_url + ":" + host_port
+print "Beginning symlinking."
+print "Looking up from MythTV: http://" + url + '/Dvr/GetRecordedList'
 
-tree = ET.parse(urllib.urlopen("http://" + host_url + ":" + host_port + '/Dvr/GetRecordedList'))
+tree = ET.parse(urllib.urlopen("http://" + url + '/Dvr/GetRecordedList'))
 root = tree.getroot()
 
 for program in root.iter('Program'):
 
-	title = program.find('Title').text
-	ep_title = program.find('SubTitle').text
-	ep_season = program.find('Season').text.zfill(2)
-	ep_num = program.find('Episode').text.zfill(2)
-	ep_file_extension= program.find('FileName').text[-4:]
-	ep_file_name = program.find('FileName').text
-        ep_id = program.find('ProgramId').text
+    title = program.find('Title').text
+    ep_title = program.find('SubTitle').text
+    ep_season = program.find('Season').text.zfill(2)
+    ep_num = program.find('Episode').text.zfill(2)
+    ep_file_extension = program.find('FileName').text[-4:]
+    ep_file_name = program.find('FileName').text
+    ep_id = program.find('ProgramId').text
 
-        # Skip previously finished files
-        if len(lib) > 0:
-            if ep_id in lib:
-		    print "Matched program ID, skipping " + title + " - S" + ep_season + "E" + ep_num
-		    continue
-            elif len(ep_id) > 0:
-                lib.append(ep_id)
-    
-	# Plex doesn't do specials
-	if ep_season == '00' and ep_num == '00':
-		continue
+    episode_name = title + " - S" + ep_season + "E" + ep_num
+    # Skip previously finished files
+    if len(lib) > 0:
+        if ep_id in lib:
+            print "Matched program ID, skipping " + episode_name
+            continue
+        elif len(ep_id) > 0:
+            lib.append(ep_id)
 
-	# Symlink path
-	link_path = plex_library_directory + title + separator + title + " - S" + ep_season + "E" + ep_num + ep_file_extension
+    # Plex doesn't do specials
+    if ep_season == '00' and ep_num == '00':
+        continue
 
-	# Watch for oprhaned recordings!
-	source_dir = None
-	for myth_dir in mythtv_recording_directories[:]:
-		source_path = myth_dir + ep_file_name
-		if os.path.isfile(source_path):
-			source_dir = myth_dir
-			break
+    # Symlink path
+    link_path = (plex_library_directory +
+                 title + separator + episode_name + ep_file_extension)
 
-	if source_dir is None:
-		print "Cannot create symlink for " + title + " S" + ep_season + "E" + ep_num + ", no valid source directory.  Skipping."
-		continue
+    # Watch for oprhaned recordings!
+    source_dir = None
+    for myth_dir in mythtv_recording_directories[:]:
+        source_path = myth_dir + ep_file_name
+        if os.path.isfile(source_path):
+            source_dir = myth_dir
+            break
 
-	if os.path.exists(link_path) or os.path.islink(link_path):
-		print "Symlink " + link_path + " already exists.  Skipping."
-		continue
+    if source_dir is None:
+        print ("Cannot create symlink for "
+               + episode_name + ", no valid source directory.  Skipping.")
+        continue
 
-	if not os.path.exists(plex_library_directory + title):
-		print "Show folder does not exist, creating."
-		os.makedirs(plex_library_directory + title)
+    if os.path.exists(link_path) or os.path.islink(link_path):
+        print "Symlink " + link_path + " already exists.  Skipping."
+        continue
 
-	print "Linking " + source_path + " ==> " + link_path
-	os.symlink(source_path, link_path)
+    if not os.path.exists(plex_library_directory + title):
+        print "Show folder does not exist, creating."
+        os.makedirs(plex_library_directory + title)
+
+    print "Linking " + source_path + " ==> " + link_path
+    os.symlink(source_path, link_path)
 
 # Save the list of file IDs
 outstring = ""
