d98cfa0509a378add13935f8553fef3dac5e0c66
Andrew Scagnelli <andrew.scagnelli@gmail.com>
2014-09-08 19:19:48 -0400

    Separating configuration into its own file.

---
+++ config.py
@@ -0,0 +1,16 @@
+# IP or hostname of the mythTV server
+host_url = "10.0.1.10"
+
+# Port number used by the mythTV server
+host_port = "6544"
+
+# Path to the location of links
+plex_library_directory = "/home/ascagnel/TV Shows/"
+
+# A list of the mythTV recording directories.
+mythtv_recording_directories = ["/var/media/disk1/Recordings/",
+                                "/var/media/disk2/Recordings/",
+                                "/var/media/disk3/Recordings/",
+                                "/var/media/disk4/Recordings/"]
+
+
