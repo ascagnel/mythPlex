93eff6f2df4972544398573ee2c931f14e6ea3db
Andrew Scagnelli <andrew.scagnelli@gmail.com>
2014-12-06 01:25:15 -0500

    Fixing bug with filename.

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ host_url = "localhost"
 host_port = "6544"
 
 # Path to the location of links
-plex_tv_directory = "~/TV Shows/"
-plex_movie_directory = "~/Movies/"
-plex_specials_directory = "~/TV Shows/Specials/"
+plex_tv_directory = "/var/media/disk3/TV/"
+plex_movie_directory = "/var/media/disk3/Movies/"
+plex_specials_directory = "/var/media/disk3/TV/"
 
 # A list of the mythTV recording directories.
-mythtv_recording_directories = ["/var/media/disk1/Recordings/"]
+mythtv_recording_directories = ["/var/media/disk1/Recordings/","/var/media/disk2/Recordings/","/var/media/disk3/Recordings/","/var/media/disk4/Recordings/"]
 
 moviedb_enabled = False
 moviedb_api_key = ""
@@ -18,9 +18,8 @@ moviedb_testmode = False
 
 avconv_enabled = False
 avconv_bitrate = "8300k"
-avconv_mythcommflag_enabled = False
-avconv_mythcommflag_verbose = False
-avconv_audio_offset_enabled = False
+avconv_mythcommflag_enabled = True
+avconv_mythcommflag_verbose = True
+avconv_audio_offset_enabled = True
 avconv_audio_offset_time = 0.2
-avconv_remux_enabled = False
-
+avconv_remux_enabled = True
