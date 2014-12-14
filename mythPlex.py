0f6e71d1e8400c78e065959d8a6504d35f7dcaf5
Andrew Scagnelli <andrew.scagnelli@gmail.com>
2014-12-14 12:39:45 -0500

    Code cleanup.

--- mythPlex.py
+++ mythPlex.py
@@ -6,13 +6,12 @@ import urllib
 import platform
 import re
 import config
-import cgi
-import sys
 from MythTV.tmdb3 import searchMovie
 from MythTV.tmdb3 import set_key
 import calendar
 from datetime import datetime, timedelta
 import commands
+
 
 def utc_to_local(utc_dt):
     # get integer timestamp to avoid precision lost
@@ -20,6 +19,7 @@ def utc_to_local(utc_dt):
     local_dt = datetime.fromtimestamp(timestamp)
     assert utc_dt.resolution >= timedelta(microseconds=1)
     return local_dt.replace(microsecond=utc_dt.microsecond)
+
 
 def open_library():
     if os.path.isfile('.library') or os.path.islink('.library'):
@@ -29,6 +29,7 @@ def open_library():
     else:
         lib = []
     return lib
+
 
 def main():
     print "[INFO] Starting mythEx"
@@ -42,15 +43,15 @@ def main():
     for program in root.iter('Program'):
 
         title = program.find('Title').text
-        ep_title = program.find('SubTitle').text
         ep_season = program.find('Season').text.zfill(2)
         ep_num = program.find('Episode').text.zfill(2)
         ep_file_extension = program.find('FileName').text[-4:]
         ep_file_name = program.find('FileName').text
         ep_id = program.find('ProgramId').text
-        ep_airdate = program.find('Airdate').text
         ep_temp = program.find('StartTime').text
-        ep_start_time = utc_to_local(datetime.strptime(ep_temp, '%Y-%m-%dT%H:%M:%SZ'))
+        ep_start_time = utc_to_local(datetime.strptime(
+                                     ep_temp,
+                                     '%Y-%m-%dT%H:%M:%SZ'))
         # parse start time for file-system safe name
         ep_start_time = datetime.strftime(ep_start_time, '%Y-%m-%d %H%M')
 
@@ -62,10 +63,12 @@ def main():
 
         # Skip previously finished files
         if ep_id in lib:
-            print "[WARN] Matched program ID" + ep_id + ", skipping " + episode_name
+            print ("[WARN] Matched program ID" + ep_id +
+                   ", skipping " + episode_name)
             continue
         elif ep_id is not None:
-            print "[INFO] Adding " + episode_name + " to library [" + ep_id + "]"
+            print ("[INFO] Adding " + episode_name +
+                   " to library [" + ep_id + "]")
             lib.append(ep_id)
 
         # Handle specials, movies, etc.
@@ -81,32 +84,34 @@ def main():
                 if (len(res) is 0):
                     moviedb_successful = False
                     print "[WARN] " + episode_name + "not found in TheMovieDB"
-                else: 
+                else:
                     print "[INFO] Successfully looked up in MovieDB"
                     moviedb_successful = True
                     print (res[0].title)
-                    title = re.sub('[\[\]/\\;><&*%=+@!#^()|?]', '_', res[0].title)
+                    title = re.sub('[\[\]/\\;><&*%=+@!#^()|?]',
+                                   '_', res[0].title)
                     episode_name = title
                     link_path = (config.plex_movie_directory +
-                              title + separator + episode_name + ep_file_extension)
+                                 title + separator + episode_name +
+                                 ep_file_extension)
 
-        #Fallback 2: start time 
-            if (ep_start_time is not None): 
+        #Fallback 2: start time
+            if (ep_start_time is not None):
                 if (moviedb_run is False or (moviedb_run is True and
                                              moviedb_successful is False)):
                     print "[INFO] (fallback 2) using start time"
                     episode_name = title + " - " + ep_start_time
                     print "[INFO] Changed to " + episode_name
                     link_path = (config.plex_specials_directory +
-                              title + separator + episode_name + ep_file_extension)
+                                 title + separator + episode_name +
+                                 ep_file_extension)
             else:
                 print "[WARN] no start time available"
 
         else:
             print "[INFO] have season and episode."
             link_path = (config.plex_tv_directory +
-                     title + separator + episode_name + ep_file_extension)
-
+                         title + separator + episode_name + ep_file_extension)
 
         # Symlink path
         print "[INFO] symlink processing.."
@@ -128,23 +133,25 @@ def main():
             print "[WARN] Symlink " + link_path + " already exists.  Skipping."
             continue
 
-        if (config.plex_tv_directory in link_path) and (not os.path.exists(config.plex_tv_directory + title)):
-            print "[INFO] Show folder does not exist, creating."
-            os.makedirs(config.plex_tv_directory + title)
+        if (config.plex_tv_directory in link_path):
+            if (not os.path.exists(config.plex_tv_directory + title)):
+                print "[INFO] Show folder does not exist, creating."
+                os.makedirs(config.plex_tv_directory + title)
 
-        if (config.plex_movie_directory in link_path) and (not os.path.exists(config.plex_movie_directory + title)):
-            print "[INFO] Show folder does not exist, creating."
-            os.makedirs(config.plex_movie_directory + title)
+        if (config.plex_movie_directory in link_path):
+            if (not os.path.exists(config.plex_movie_directory + title)):
+                print "[INFO] Show folder does not exist, creating."
+                os.makedirs(config.plex_movie_directory + title)
 
-        if (config.plex_specials_directory in link_path) and (not os.path.exists(config.plex_specials_directory + title)):
-            print "[INFO] Show folder does not exist, creating."
-            os.makedirs(config.plex_specials_directory + title)
+        if (config.plex_specials_directory in link_path):
+            if (not os.path.exists(config.plex_specials_directory + title)):
+                print "[INFO] Show folder does not exist, creating."
+                os.makedirs(config.plex_specials_directory + title)
 
         # avconv (next-gen ffmpeg) support -- convert files to MP4
         # so smaller devices (eg Roku, AppleTV, FireTV, Chromecast)
         # support native playback.
         if config.avconv_enabled is True:
-            mthcommflag_exists = False
             # MythTV's mythcommflag can be used to remove commercials,
             # shrinking recordings and improving viewing.
             if config.avconv_mythcommflag_enabled is True:
@@ -152,7 +159,7 @@ def main():
 
             # Re-encode with avconv
             run_avconv(source_path, link_path)
-            
+
         elif config.avconv_remux_enabled:
             if config.avconv_mythcommflag_enabled is True:
                 run_avconv_remux_mythcommflag(source_path, link_path)
@@ -164,6 +171,7 @@ def main():
             os.symlink(source_path, link_path)
     close_library(lib)
 
+
 def close_library(lib):
     # Save the list of file IDs
     outstring = ",".join(lib)
@@ -171,6 +179,7 @@ def close_library(lib):
     library = open('.library', 'w')
     library.write(outstring)
     library.close()
+
 
 def run_mythcommflag(source_path):
     mythcommflag_command = 'mythcommflag -f '
@@ -183,6 +192,7 @@ def run_mythcommflag(source_path):
         mythcommflag_command += ' -v'
     print "[INFO] Running mythcommflag: {" + mythcommflag_command + "}"
     os.system(mythcommflag_command)
+
 
 def run_avconv(source_path, output_path):
     avconv_command = "nice -n " + str(avconv_nicevalue)
@@ -207,10 +217,13 @@ def run_avconv(source_path, output_path):
     print "Running avconv with command line " + avconv_command
     os.system(avconv_command)
 
+
 def run_avconv_remux(source_path, output_path):
-    avconv_command = "avconv -i " + source_path + " -c copy \"" + output_path + "\""
+    avconv_command = ("avconv -i " + source_path + " -c copy \"" +
+                      output_path + "\"")
     print "Running avconv remux with command " + avconv_command
     os.system(avconv_command)
+
 
 def run_avconv_remux_mythcommflag(source_path, output_path):
     print "[INFO] Running mythcommflag on {" + source_path + "}"
@@ -221,21 +234,23 @@ def run_avconv_remux_mythcommflag(source_path, output_path):
     starts_with_commercial = False
     for cutpoint in cutlist:
         if 'framenum' in cutpoint:
-	    line = cutpoint.split()
-            print '[INFO] ' + line[0] + ' - {' + line[1] + '} -- ' + line[2] + ' - {' + line[3] + '}'
+            line = cutpoint.split()
+            print ('[INFO] ' + line[0] + ' - {' + line[1] + '} -- ' +
+                   line[2] + ' - {' + line[3] + '}')
             if line[1] is '0' and line[3] is '4':
                 starts_with_commercial = True
             cutpoints.append(line[1])
             pointtypes.append(line[3])
     cutlist.close()
     os.system('rm .mythExCommflag.edl')
-    framerate = float(commands.getoutput('ffmpeg -i '+source_path+' 2>&1 | sed -n \"s/.*, \\(.*\\) fp.*/\\1/p\"'))
+    framerate = float(commands.getoutput('ffmpeg -i ' + source_path +
+                      ' 2>&1 | sed -n \"s/.*, \\(.*\\) fp.*/\\1/p\"'))
     print '[INFO] Video frame rate is ' + str(framerate)
     print '[INFO] Starts with commercial? ' + str(starts_with_commercial)
     print '[INFO] Found ' + str(len(cutpoints)) + ' cut points.'
     segments = 0
     for cutpoint in cutpoints:
-        index = cutpoints.index(cutpoint) 
+        index = cutpoints.index(cutpoint)
         startpoint = float(cutpoints[index])/framerate
         duration = 0
         if index is 0 and not starts_with_commercial:
@@ -246,14 +261,21 @@ def run_avconv_remux_mythcommflag(source_path, output_path):
             print '[INFO] Skipping cut point type 4'
             continue
         elif (index+1) < len(cutpoints):
-            duration = (float(cutpoints[index+1])-float(cutpoints[index]))/framerate
+            duration = (float(cutpoints[index+1]) -
+                        float(cutpoints[index]))/framerate
         print '[INFO] Start point: [' + str(startpoint) + ']'
-        print '[INFO] duration of segment ' + str(segments) + ': ' + str(duration)
+        print ('[INFO] duration of segment ' +
+               str(segments) + ': ' + str(duration))
         if duration is 0:
-            avconv_command = 'avconv -i ' + source_path + ' -ss ' + str(startpoint) + ' -c copy output' + str(segments) + '.mpg'
+            avconv_command = ('avconv -i ' + source_path + ' -ss ' +
+                              str(startpoint) + ' -c copy output' +
+                              str(segments) + '.mpg')
         else:
-            avconv_command = 'avconv -i ' + source_path + ' -ss ' + str(startpoint) + ' -t ' + str(duration) + ' -c copy output' + str(segments) + '.mpg'
-        print '[INFO] running avconv with command line {' + avconv_command + '}'
+            avconv_command = ('avconv -i ' + source_path + ' -ss ' +
+                              str(startpoint) + ' -t ' + str(duration) +
+                              ' -c copy output' + str(segments) + '.mpg')
+        print ('[INFO] running avconv with command line {' +
+               avconv_command + '}')
         os.system(avconv_command)
         segments = segments + 1
     current_segment = 0
@@ -290,14 +312,12 @@ else:
 
 if config.moviedb_enabled:
     set_key(config.moviedb_api_key)
-    if config.moviedb_testmode:
-        moviedb = 'http://private-dc013f25e-themoviedb.apiary-mock.com/3/search/movie'
-    else:
-        moviedb = 'http://api.themoviedb.org/3/search/movie'
+    moviedb = 'http://api.themoviedb.org/3/search/movie'
 
 if platform.system() == "Windows":
     separator = "\\"
 else:
     separator = "/"
 
-if __name__ == "__main__" : main()
+if __name__ == "__main__":
+    main()
