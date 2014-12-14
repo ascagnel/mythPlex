8e84788ae7af9d391c35c9a67841944012243a55
Andrew Scagnelli <andrew.scagnelli@gmail.com>
2014-12-14 12:39:30 -0500

    Fixing config file.

--- config.py
+++ config.py
@@ -5,21 +5,20 @@ host_url = "localhost"
 host_port = "6544"
 
 # Path to the location of links
-plex_tv_directory = "/var/media/disk3/TV/"
-plex_movie_directory = "/var/media/disk3/Movies/"
-plex_specials_directory = "/var/media/disk3/TV/"
+plex_tv_directory = "~/TV Shows/"
+plex_movie_directory = "~/Movies/"
+plex_specials_directory = "~/TV Shows/Specials/"
 
 # A list of the mythTV recording directories.
-mythtv_recording_directories = ["/var/media/disk1/Recordings/","/var/media/disk2/Recordings/","/var/media/disk3/Recordings/","/var/media/disk4/Recordings/"]
+mythtv_recording_directories = ["/var/media/disk1/Recordings/"]
 
 moviedb_enabled = False
 moviedb_api_key = ""
-moviedb_testmode = False
 
 avconv_enabled = False
+avconv_remux_enabled = False
 avconv_bitrate = "8300k"
 avconv_mythcommflag_enabled = True
 avconv_mythcommflag_verbose = True
 avconv_audio_offset_enabled = True
 avconv_audio_offset_time = 0.2
-avconv_remux_enabled = True
