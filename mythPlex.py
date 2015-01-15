adc1b70260cf9792348e68cdd514576467597b74
Andrew Scagnelli <andrew.scagnelli@gmail.com>
2015-01-14 22:54:02 -0500

    Fixing missing close paren
    Updating status check for successful NTSC lookup.

--- mythPlex.py
+++ mythPlex.py
@@ -183,11 +183,11 @@ def mythcommflag_run(source_path):
     avconv_fps = subprocess.Popen(['avconv', '-i', source_path],
                                   stdout=subprocess.PIPE,
                                   stderr=subprocess.PIPE).communicate()[1]
-    if (fps_pattern.search(str(avconv_fps)).groups()):
+    if (fps_pattern.search(str(avconv_fps))):
         framerate = float(fps_pattern.search(str(avconv_fps)).groups()[0])
     else:
         logger.info("Could not look up FPS, trying PAL format (25FPS).")
-        fps_pattern = re.compile(r'(\d{2} fps')
+        fps_pattern = re.compile(r'(\d{2}) fps')
         framerate = float(fps_pattern.search(str(avconv_fps)).groups()[0])
 
     logger.debug("Video frame rate: %s", str(framerate))
